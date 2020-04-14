2060651bd0ec4537c87e8ab0978ac4c22e5c62ba
Dongbin Na <ndb796@naver.com>
2020-04-14 23:08:51 +0900

    Update solver.py

--- stargan/solver.py
+++ stargan/solver.py
@@ -1034,8 +1034,10 @@ class Solver(object):
             result_path = os.path.join(self.result_dir, '{}-images.jpg'.format(i+1))
             save_image(self.denorm(x_concat.data.cpu()), result_path, nrow=1, padding=0)
 
-            if i == 2:     # stop after this many images
+            '''
+            if i == 49:     # stop after this many images
                 break
+            '''
         
         # Print metrics
         print('{} images. L1 error: {}. L2 error: {}. prop_dist: {}. L0 error: {}. L_-inf error: {}.'.format(n_samples, 
